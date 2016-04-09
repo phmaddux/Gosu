9174faf22e9e9fd6332dd3e8ba708307fbb32c70
Hunter Maddux <phmaddux@gmail.com>
2016-04-08 20:57:59 -0400

    Added 2 images and added draw method

--- WhackARuby.rb
+++ WhackARuby.rb
@@ -5,7 +5,15 @@ class WhackARuby < Gosu::Window
   def initialize
     super(800, 600)
     self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+  end
 
+  def draw
+    @image.draw(@x - @width / 2, @y - @height / 2, 1)
   end
 
 end
