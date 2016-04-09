945f1e633f45255f069d1cbd7bd21d6226a1d110
Hunter Maddux <phmaddux@gmail.com>
2016-04-08 21:25:43 -0400

    added boundaries and rebounding

--- WhackARuby.rb
+++ WhackARuby.rb
@@ -10,12 +10,22 @@ class WhackARuby < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
   end
 
   def draw
     mid_x = @x - @width / 2
     mid_y = @y - @height / 2
     @image.draw(mid_x, mid_y, 1)
+  end
+
+  def update
+    @x += @velocity_x
+    @y = @y + @velocity_y
+    #These ^^ two lines += and = y +  mean the same thing
+    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
   end
 
 end
