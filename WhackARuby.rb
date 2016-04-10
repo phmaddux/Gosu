0d65a70437a6c78f4aaee7c5d22b1811609b04b6
Hunter Maddux <phmaddux@gmail.com>
2016-04-10 19:51:02 -0400

    Dissappearing ruby

--- WhackARuby.rb
+++ WhackARuby.rb
@@ -12,12 +12,15 @@ class WhackARuby < Gosu::Window
     @height = 43
     @velocity_x = 5
     @velocity_y = 5
+    @visible = 0
   end
 
   def draw
     mid_x = @x - @width / 2
     mid_y = @y - @height / 2
-    @image.draw(mid_x, mid_y, 1)
+    if @visible > 0
+      @image.draw(mid_x, mid_y, 1)
+    end
   end
 
   def update
@@ -25,14 +28,10 @@ class WhackARuby < Gosu::Window
     @y = @y + @velocity_y
     #These ^^ two lines += and = y +  mean the same thing
     @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
-
-    hits_bottom = (@y + @height / 2 > 600)
-    hits_top    = (@y - @height / 2 < 0)
-    if hits_bottom || hits_top
-      @velocity_y = @velocity_y * -1 #reverse volocity!
-    end
+    @velocity_y *= -1 if (@y + @height / 2 > 600) || (@y - @height / 2 < 0)
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
-
 end
 
 window = WhackARuby.new
